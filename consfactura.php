32f7b584fafe9043286706222b58fe9109c4063a
Jesus Maldonado <jmaldonadoca@gmail.com>
2014-03-27 07:25:02 -0600

    version con consulta de facturas modificada para seleccionar ultimos 30 dias por default y tablas a excel con bordes.

--- consfactura.php
+++ consfactura.php
@@ -91,13 +91,14 @@
 		 t4.nom_corto, t5.nom_corto,t1.cant, t1.observaciones FROM facturas AS t1 INNER JOIN clientes AS t2 ON t1.idclientes=t2.idclientes 
 		 LEFT JOIN succliente as t3 on t1.idsuccliente=t3.no_almacen LEFT JOIN representantes as t4 ON t1.agente = t4.idrepresentantes
 		 LEFT JOIN productos as t5 ON t1.idproductos = t5.idproductos
-		 WHERE fecha >='$fechainic' AND fecha <='$fechafin'";
+		 WHERE fecha >='$fechainic' AND fecha <='$fechafin' ORDER BY t1.no_factura";
 		}else{
 				//SI NO SE ELIGIERON
+			$fechamin = date(DATE_ATOM);
 			$sqlCommand = "SELECT t1.no_factura,t1.oc,t1.remision,t1.fecha,t2.razon_social,t3.nom_sucursal,t1.subtotal,t1.iva,t1.total,
 		t4.nom_corto, t5.nom_corto,t1.cant,t1.observaciones FROM facturas AS t1 INNER JOIN clientes AS t2 ON t1.idclientes=t2.idclientes LEFT JOIN succliente as t3 on 
 		t1.idsuccliente=t3.no_almacen LEFT JOIN representantes as t4 ON t1.agente = t4.idrepresentantes
-		LEFT JOIN productos as t5 ON t1.idproductos = t5.idproductos";
+		LEFT JOIN productos as t5 ON t1.idproductos = t5.idproductos WHERE DATE_SUB(CURDATE(),INTERVAL 30 DAY) <= t1.fecha  ORDER BY t1.no_factura";
 			
 		}
 		
@@ -105,9 +106,9 @@
 		 $query1=mysqli_query($mysqli, $sqlCommand) or die ("ERROR EN CONSULTA DE INVENTARIOS CLIENTES. ".mysqli_error($mysqli));
 		 //construccion de la casilla remision/oc
 		 
-		 echo "	<table class= 'aexcel' id='tablaaxl'>";
+		 echo "	<table border=1 id='tablaaxl' class='aexcel'>";
 		 echo "<tr><th>FACTURA</th><th>REM/OC</th><th>FECHA</th><th>CLIENTE</th><th>SUCURSAL</th></th><th>AGENTE</th><th>SUBTOTAL</th>
-		 <th>IVA</th><th>TOTAL</th><th>PRODUCTO</th><th>CANT.</th><th>OBS.</th></tr>";
+		 <th>IVA</th><th>TOTAL</th><th>PRODUCTO</th><th>PIEZAS</th><th>OBS.</th></tr>";
 		 
 				 while($row2=mysqli_fetch_row($query1)){
 				 	$comb = $row2[2]."/".$row2[1];
