99fd330148579e7064df12bf6cb25dbca377819c
Jesus Maldonado <jmaldonadoca@gmail.com>
2014-05-14 23:16:55 -0500

    se corrige error en la consulta ajax de clientes para que acepte la ñ.

--- get_client_list.php
+++ get_client_list.php
@@ -15,6 +15,7 @@
     }
 	
 	
+	
     $req = "SELECT idclientes,nom_corto,razon_social,rfc,calleno,col,del,ciudad,estado,cp,nivel FROM clientes WHERE nom_corto like '" 
     . mysqli_real_escape_string($mysqli,$_GET['term']) . "%' && status <> 2"; 
 	
@@ -23,9 +24,9 @@
     while($row = mysqli_fetch_array($query))
     {
     	$domi1 = $row['calleno']." COL.".$row['col']."  DEL.".$row['del']." C.P.".$row['cp']." ".$row['ciudad'].",".$row['estado'];
-        $results[] = array('label' => $row['nom_corto'],'idclientes' 
+        $results[] = array_map('utf8_encode',array('label' => $row['nom_corto'],'idclientes' 
         => $row['idclientes'],'razon'=>$row['razon_social'],'rfc'=>$row['rfc'],
-		'domicilio'=> $domi1,'nivel'=>$row['nivel']);
+		'domicilio'=> $domi1,'nivel'=>$row['nivel']));
     }
 	
 	
@@ -33,7 +34,10 @@
 	    mysqli_free_result($query);
 	    /* cerrar la conexion */
 	    mysqli_close($mysqli);
-    
+		/*funcion de conversion de caracteres */
+		
+    	
+		
     	echo json_encode($results);
 
 
